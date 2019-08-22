a290560d7a02f181739a0b064a5dda2f3104df34
tanakataro1129 <tanakataro1129@example.com>
2019-08-22 17:22:55 +0900

    Fix: 乱数に偏り追加

--- src/scripts/dummy-record.ts
+++ src/scripts/dummy-record.ts
@@ -8,15 +8,19 @@ function rand(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
 };
 
+function randSum(max: number) {
+    return Math.floor((Math.random() + Math.random()) / 2 * max);
+}
+
 var stages = [new Stage(BaseStage.CastleOfAquaBlue, '1'), new Stage(BaseStage.CastleOfAquaBlue, '2'), new Stage(BaseStage.CastleOfAquaBlue, '3'),];
 var ranks = [new PlayerRank(RankClass.SuperStar, RankLevel.C), new PlayerRank(RankClass.SuperStar, RankLevel.B), new PlayerRank(RankClass.SuperStar, RankLevel.A),]
 
 export function createDummy(size: number) {
     var records = [];
     for (var i = 0; i < size; i++) {
-        var girl = _girls[rand(_girls.length)];
-        var stage = stages[rand(stages.length)];
-        var rank = ranks[rand(ranks.length)];
+        var girl = _girls[randSum(_girls.length)];
+        var stage = stages[randSum(stages.length)];
+        var rank = ranks[randSum(ranks.length)];
         var victory = (rand(3) > 1) ? Victory.Win : Victory.Lose;
         records.push(
             new WarRecord(
